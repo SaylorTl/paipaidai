8016e065bf9c37e00355733b616dae9e7000aa49
shitingle <694475241@qq.com>
2018-03-01 14:57:52 +0800

    fxed

---
+++ core/global.php
@@ -0,0 +1,19 @@
+<?php
+//请替换自己的appid和appPrivateKey, 申请地址：http://open.ppdai.com/
+$appid="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+/**
+ * php在生成密钥时，需要选择PEM PKCS#1格式的密钥，否则会报错
+ * @var string $appPrivateKey
+ * @var string $appPublicKey
+ */
+$appPrivateKey ="
+-----BEGIN RSA PRIVATE KEY-----
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+-----END RSA PRIVATE KEY-----
+";
+$appPublicKey ="
+-----BEGIN PUBLIC KEY-----
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+-----END PUBLIC KEY-----
+";
